3a4b284a6d80d1053c386136daacf81c7aff2b76
tupez <cesar.gl@gmail.com>
2025-05-07 01:30:40 -0700

    Update Program.cs

--- Labfiles/01-analyze-images/C-Sharp/image-analysis/Program.cs
+++ Labfiles/01-analyze-images/C-Sharp/image-analysis/Program.cs
@@ -1,3 +1,5 @@
+// Import namespaces
+using Azure.AI.Vision.ImageAnalysis;
 using System;
 using System.IO;
 using System.Net.Http;
@@ -37,6 +39,10 @@ namespace image_analysis
                 }
 
                 // Authenticate Azure AI Vision client
+                ImageAnalysisClient client = new ImageAnalysisClient(
+                        new Uri(aiSvcEndpoint),
+                            new AzureKeyCredential(aiSvcKey));
+                
 
                 
                 // Analyze image
@@ -58,10 +64,104 @@ namespace image_analysis
                                                      FileMode.Open);
 
             // Get result with specified features to be retrieved
+            ImageAnalysisResult result = client.Analyze(
+                    BinaryData.FromStream(stream),
+                        VisualFeatures.Caption | 
+                            VisualFeatures.DenseCaptions |
+                                VisualFeatures.Objects |
+                                    VisualFeatures.Tags |
+                                        VisualFeatures.People);
+            
             
             
             // Display analysis results
+            // Get image captions
+            if (result.Caption.Text != null)
+            {
+                    Console.WriteLine(" Caption:");
+                        Console.WriteLine($"   \"{result.Caption.Text}\", Confidence {result.Caption.Confidence:0.00}\n");
+            }
+
+            // Get image dense captions
+            Console.WriteLine(" Dense Captions:");
+            foreach (DenseCaption denseCaption in result.DenseCaptions.Values)
+            {
+                    Console.WriteLine($"   Caption: '{denseCaption.Text}', Confidence: {denseCaption.Confidence:0.00}");
+            }
+
+            // Get image tags
+            if (result.Tags.Values.Count > 0)
+            {
+                    Console.WriteLine($"\n Tags:");
+                        foreach (DetectedTag tag in result.Tags.Values)
+                            {
+                                        Console.WriteLine($"   '{tag.Name}', Confidence: {tag.Confidence:F2}");
+                            }
+                            }
+                            
             
+
+
+            // Get objects in the image
+            if (result.Objects.Values.Count > 0)
+            {
+                    Console.WriteLine(" Objects:");
+
+                        // Prepare image for drawing
+                            stream.Close();
+                                System.Drawing.Image image = System.Drawing.Image.FromFile(imageFile);
+                                    Graphics graphics = Graphics.FromImage(image);
+                                        Pen pen = new Pen(Color.Cyan, 3);
+                                            Font font = new Font("Arial", 16);
+                                                SolidBrush brush = new SolidBrush(Color.WhiteSmoke);
+
+                                                    foreach (DetectedObject detectedObject in result.Objects.Values)
+                                                        {
+                                                                    Console.WriteLine($"   \"{detectedObject.Tags[0].Name}\"");
+
+                                                                            // Draw object bounding box
+                                                                                    var r = detectedObject.BoundingBox;
+                                                                                            Rectangle rect = new Rectangle(r.X, r.Y, r.Width, r.Height);
+                                                                                                    graphics.DrawRectangle(pen, rect);
+                                                                                                            graphics.DrawString(detectedObject.Tags[0].Name,font,brush,(float)r.X, (float)r.Y);
+                                                        }
+
+                                                            // Save annotated image
+                                                                String output_file = "objects.jpg";
+                                                                    image.Save(output_file);
+                                                                        Console.WriteLine("  Results saved in " + output_file + "\n");
+            }
+                                         
+            // Get people in the image
+            if (result.People.Values.Count > 0)
+            {
+                    Console.WriteLine($" People:");
+
+                        // Prepare image for drawing
+                            System.Drawing.Image image = System.Drawing.Image.FromFile(imageFile);
+                                Graphics graphics = Graphics.FromImage(image);
+                                    Pen pen = new Pen(Color.Cyan, 3);
+                                        Font font = new Font("Arial", 16);
+                                            SolidBrush brush = new SolidBrush(Color.WhiteSmoke);
+
+                                                foreach (DetectedPerson person in result.People.Values)
+                                                    {
+                                                                // Draw object bounding box
+                                                                        var r = person.BoundingBox;
+                                                                                Rectangle rect = new Rectangle(r.X, r.Y, r.Width, r.Height);
+                                                                                        graphics.DrawRectangle(pen, rect);
+                                                                                                
+                                                                                                        // Return the confidence of the person detected
+                                                                                                        Console.WriteLine($"   Bounding box {person.BoundingBox.ToString()}, Confidence: {person.Confidence:F2}");
+                                                    }
+
+                                                        // Save annotated image
+                                                            String output_file = "persons.jpg";
+                                                                image.Save(output_file);
+                                                                    Console.WriteLine("  Results saved in " + output_file + "\n");
+            }
+                                                    
+                    
 
         }
     }
