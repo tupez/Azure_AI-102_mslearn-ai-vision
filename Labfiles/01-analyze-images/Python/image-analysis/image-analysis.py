53c02122fab7db4a436ffd4894839c1f55ec372f
tupez <cesar.gl@gmail.com>
2025-05-07 01:31:56 -0700

    Update image-analysis.py

--- Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
+++ Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
@@ -1,3 +1,8 @@
+# import namespaces
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
+
 from dotenv import load_dotenv
 import os
 from PIL import Image, ImageDraw
@@ -30,6 +35,11 @@ def main():
             image_data = f.read()
 
         # Authenticate Azure AI Vision client
+        cv_client = ImageAnalysisClient(
+                endpoint=ai_endpoint,
+                    credential=AzureKeyCredential(ai_key)
+                    )
+        
 
         
         # Analyze image
@@ -44,7 +54,16 @@ def AnalyzeImage(image_filename, image_data, cv_client):
 
     try:
         # Get result with specified features to be retrieved
-        
+        result = cv_client.analyze(
+                image_data=image_data,
+                    visual_features=[
+                                VisualFeatures.CAPTION,
+                                        VisualFeatures.DENSE_CAPTIONS,
+                                                VisualFeatures.TAGS,
+                                                        VisualFeatures.OBJECTS,
+                                                                VisualFeatures.PEOPLE],
+                                                                )
+                
 
     except HttpResponseError as e:
         print(f"Status code: {e.status_code}")
@@ -52,6 +71,79 @@ def AnalyzeImage(image_filename, image_data, cv_client):
         print(f"Message: {e.error.message}")
 
     # Display analysis results
+    # Get image captions
+        if result.caption is not None:
+                print("\nCaption:")
+                print(" Caption: '{}' (confidence: {:.2f}%)".format(result.caption.text, result.caption.confidence * 100))
+
+                # Get image dense captions
+                if result.dense_captions is not None:
+                        print("\nDense Captions:")
+                        for caption in result.dense_captions.list:
+                                        print(" Caption: '{}' (confidence: {:.2f}%)".format(caption.text, caption.confidence * 100))
+
+                                        # Get image tags
+                                        if result.tags is not None:
+                                            print("\nTags:")
+                                            for tag in result.tags.list:
+                                                print(" Tag: '{}' (confidence: {:.2f}%)".format(tag.name, tag.confidence * 100))
+
+
+                                        # Get objects in the image
+                                        if result.objects is not None:
+                                                print("\nObjects in image:")
+
+                                                # Prepare image for drawing
+                                                image = Image.open(image_filename)
+                                                fig = plt.figure(figsize=(image.width/100, image.height/100))
+                                                plt.axis('off')
+                                                draw = ImageDraw.Draw(image)
+                                                color = 'cyan'
+
+                                                for detected_object in result.objects.list:
+                                                        # Print object name
+                                                        print(" {} (confidence: {:.2f}%)".format(detected_object.tags[0].name, detected_object.tags[0].confidence * 100))
+                                                                        
+                                                        # Draw object bounding box
+                                                        r = detected_object.bounding_box
+                                                        bounding_box = ((r.x, r.y), (r.x + r.width, r.y + r.height)) 
+                                                        draw.rectangle(bounding_box, outline=color, width=3)
+                                                        plt.annotate(detected_object.tags[0].name,(r.x, r.y), backgroundcolor=color)
+
+                                                        # Save annotated image
+                                                        plt.imshow(image)
+                                                        plt.tight_layout(pad=0)
+                                                        outputfile = 'objects.jpg'
+                                                        fig.savefig(outputfile)
+                                                        print('  Results saved in', outputfile)
+
+
+                                        # Get people in the image
+                                        if result.people is not None:
+                                            print("\nPeople in image:")
+
+                                            # Prepare image for drawing
+                                            image = Image.open(image_filename)
+                                            fig = plt.figure(figsize=(image.width/100, image.height/100))
+                                            plt.axis('off')
+                                            draw = ImageDraw.Draw(image)
+                                            color = 'cyan'
+
+                                            for detected_people in result.people.list:
+                                                # Draw object bounding box
+                                                r = detected_people.bounding_box
+                                                bounding_box = ((r.x, r.y), (r.x + r.width, r.y + r.height))
+                                                draw.rectangle(bounding_box, outline=color, width=3)
+
+                                                # Return the confidence of the person detected
+                                                print(" {} (confidence: {:.2f}%)".format(detected_people.bounding_box, detected_people.confidence * 100))
+                                                
+                                            # Save annotated image
+                                            plt.imshow(image)
+                                            plt.tight_layout(pad=0)
+                                            outputfile = 'people.jpg'
+                                            fig.savefig(outputfile)
+                                            print('  Results saved in', outputfile)
     
 
 
